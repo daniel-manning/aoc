9c06c55f0e4b3bb3feb278c72411dad2a50b1b29
daniel-manning <daniel.manning@digital.hmrc.gov.uk>
2018-12-19 22:59:19 +0000

    WIP Day 09

--- src/main/scala/Day09/Day09.scala
+++ src/main/scala/Day09/Day09.scala
@@ -1,12 +1,18 @@
 package Day09
 
 
-case class CircularMarbleList(pointer:Int, marbleList:List[Int]){
-  def addNextMarbleBetweenTheNextTwo(): CircularMarbleList ={
-    val nextMarble = marbleList.max + 1
+case class CircularMarbleList(pointer:Int, marbleList:List[Int], noOfPlayers:Int, currentPlayer:Int, playerScores:List[(Int, Int)]){
+  def addNextMarbleBetweenTheNextTwo(): CircularMarbleList = {
     val newMarbleList = (marbleList.slice(0, pointer+1) :+ nextMarble) ++ marbleList.slice(pointer+1, marbleList.length)
-    CircularMarbleList((pointer + 2) % marbleList.length, newMarbleList)
+
+    CircularMarbleList( pointer = (pointer + 2) % newMarbleList.length,
+                        marbleList = newMarbleList,
+                        noOfPlayers,
+                        (currentPlayer+1)%noOfPlayers,
+                        playerScores )
   }
+
+  def nextMarble:Int = marbleList.max + 1
 }
 
 object Day09 extends App {
@@ -17,7 +23,13 @@ object Day09 extends App {
 
 object Marbles {
 
+  def evolve(circularMarbleList: CircularMarbleList):CircularMarbleList = {
+    val nextMarble = circularMarbleList.nextMarble
+    circularMarbleList.addNextMarbleBetweenTheNextTwo()
+  }
 
-
+  def runTurns(circularMarbleList: CircularMarbleList, turns:Int):CircularMarbleList = {
+    (1 to turns).foldRight(circularMarbleList){(a,b) => b.addNextMarbleBetweenTheNextTwo()}
+  }
 
 }
--- src/test/scala/Day09/Day09Spec.scala
+++ src/test/scala/Day09/Day09Spec.scala
@@ -6,17 +6,35 @@ class Day09Spec extends WordSpec with Matchers{
 
   "Circular Marble List" should {
     "move two and add the next marble" in {
-      val data = CircularMarbleList(0, List(0,2,1,3))
-      val result = CircularMarbleList(2, List(0,4,2,1,3))
+      val data = CircularMarbleList(0, List(0,2,1,3), 9, 3, List())
+      val result = CircularMarbleList(2, List(0,4,2,1,3), 9, 4, List())
 
       data.addNextMarbleBetweenTheNextTwo() shouldBe result
     }
 
     "move two and add the next marble at the end of a list" in {
-      val data = CircularMarbleList(6, List(0, 4, 2, 5, 1, 6, 3))
-      val result = CircularMarbleList(2, List(0,4,2,5,1,6,3,7))
+      val data = CircularMarbleList(6, List(0, 4, 2, 5, 1, 6, 3), 9, 6, List())
+      val result = CircularMarbleList(0, List(0,4,2,5,1,6,3,7), 9, 7, List())
 
       data.addNextMarbleBetweenTheNextTwo() shouldBe result
+    }
+
+    "evolve from zero" in {
+      val result = List(0,1)
+
+      Marbles.evolve(CircularMarbleList(0, List(0), 9, 0, List())).marbleList shouldBe result
+    }
+
+    "evolve from one" in {
+      val result = CircularMarbleList(2, List(0,2,1), 9, 2, List())
+
+      Marbles.evolve(CircularMarbleList(0, List(0, 1), 9, 1, List())) shouldBe result
+    }
+
+    "evolve the pattern correctly over 15 turns" in {
+      val result = List(0, 8, 4, 9, 2, 10, 5, 11, 1, 12, 6, 13, 3, 14, 7, 15)
+
+      Marbles.runTurns(CircularMarbleList(0, List(0), 9, 0, List()), 15).marbleList shouldBe result
     }
   }
 
