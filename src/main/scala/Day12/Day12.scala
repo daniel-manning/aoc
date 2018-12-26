930298bb1bf87b50be2107a7ed9280d188efa982
daniel-manning <daniel.manning@digital.hmrc.gov.uk>
2018-12-26 18:45:38 +0000

    WIP Day 12

--- src/main/scala/Day12/Day12.scala
+++ src/main/scala/Day12/Day12.scala
@@ -17,29 +17,41 @@ object Day12 extends App {
 
   val rulesSet = ruleSetPairs.toMap
 
-  val potPositions = CellularAutomata.countPotPositions(20, rulesSet, initialState)
+  //val potPositions = CellularAutomata.countPotPositions(20, rulesSet, initialState)
+  val potPositions = CellularAutomata.countPotPositions(50000000000l, rulesSet, initialState)
 
   println(s"The sum of the positions of the evolved state: $potPositions")
 }
 
 object CellularAutomata {
-  type IndexedState = List[(Char,Int)]
+  type IndexedState = List[Int]
 
-  def countPotPositions(i: Int, ruleSet: Map[String, Char], initalState: String):Int = {
-    val states = CellularAutomata.evolve(20, ruleSet, initalState)
-    val state = states.last
+  def countPotPositions(noOfTurns: Long, ruleSet: Map[String, Char], initalState: String):Int = {
+    val state = CellularAutomata.evolve(noOfTurns, ruleSet, initalState)
 
-    state.filter(_._1 == '#').map(_._2).sum
+    state.sum
   }
 
-  def evolve(noOfTurns: Int, ruleSet: Map[String, Char], initalState: String):List[IndexedState] = {
-    (1 to noOfTurns).foldRight(List[IndexedState](indexTheInitialState(initalState))){
-      (_, b) => b :+ nextGeneration(ruleSet, b.last)
+  def indexTheInitialState(initalState: String):List[Int] = initalState.zipWithIndex.filter(p => p._1 == '#').toList.map(_._2)
+
+  def evolve(noOfTurns: Long, ruleSet: Map[String, Char], initalState: String):IndexedState = {
+    //Sigh
+    var i = 0L
+    var b = indexTheInitialState(initalState)
+    while(i < noOfTurns) {
+      b = nextGeneration(ruleSet, b)
+      i+=1
+      if(i % 10000L == 0) println(s"----------$i---------")
     }
+
+    b
+    /*(1l to noOfTurns).foldRight(indexTheInitialState(initalState)){
+      (_, b) => nextGeneration(ruleSet, b)
+    }*/
   }
 
   def nextGeneration(ruleSet: Map[String, Char], state: IndexedState):IndexedState = {
-    val headIndex = state.head._2
+/*    val headIndex = state.head._2
     val pre2 = (ruleSet.getOrElse("...." ++ state.take(1).map(_._1), '.'), headIndex - 2)
     val pre1 = (ruleSet.getOrElse("..." ++ state.take(2).map(_._1), '.'), headIndex - 1)
     val first = (ruleSet.getOrElse(".." ++ state.take(3).map(_._1), '.'), headIndex)
@@ -53,11 +65,28 @@ object CellularAutomata {
     val post1 = (ruleSet.getOrElse(state.takeRight(2).map(_._1).mkString ++ "...", '.'), lastIndex + 1)
     val post2 = (ruleSet.getOrElse(state.takeRight(1).map(_._1).mkString ++ "....", '.'), lastIndex + 2)
 
-    (pre2 :: pre1 :: first :: second :: List()).dropWhile(_._1 == '.') ++ mainSection ++ (secondLast :: last :: post1 :: post2 :: List()).reverse.dropWhile(_._1 == '.').reverse
+    (pre2 :: pre1 :: first :: second :: List()).dropWhile(_._1 == '.') ++ mainSection ++ (secondLast :: last :: post1 :: post2 :: List()).reverse.dropWhile(_._1 == '.').reverse*/
+
+    //indexed state is a list of all the plants
+    //loop through plants and generate all five positions it could be in to create set of new plants
+    //there will be duplicates but much less than the alternative
+    val newGeneration:Set[Int] =state.foldRight(Set[Int]()){
+      (a, b) => generateAllPossibleWindowsAndEvolve(a, state, ruleSet).foldRight(b){
+        (c,d) => if(c._1 == '#') d + c._2 else d
+      }
+    }
+
+    newGeneration.toList.sorted
   }
 
-  def flattenToString(input:List[(Char, Int)]):String = input.map(_._1).mkString
-
-  def indexTheInitialState(state:String):IndexedState = state.zipWithIndex.toList
+  def generateAllPossibleWindowsAndEvolve(potPosition:Int, state:IndexedState, ruleSet: Map[String, Char]):List[(Char, Int)] = {
+    List((List(potPosition, potPosition + 1, potPosition + 2, potPosition + 3, potPosition + 4), potPosition + 2),
+      (List(potPosition - 1, potPosition, potPosition + 1, potPosition + 2, potPosition + 3), potPosition + 1),
+      (List(potPosition - 2, potPosition - 1, potPosition, potPosition + 1, potPosition + 2), potPosition),
+      (List(potPosition - 3, potPosition - 2, potPosition - 1, potPosition, potPosition + 1), potPosition - 1),
+      (List(potPosition - 4, potPosition - 3, potPosition - 2, potPosition - 1, potPosition), potPosition - 2),
+    ).map(a => (a._1.map(n => if(state.contains(n)) '#' else '.').mkString, a._2))
+     .map(b => (ruleSet.getOrElse(b._1, '.'), b._2))
+  }
 
 }
--- src/test/scala/Day12/Day12Spec.scala
+++ src/test/scala/Day12/Day12Spec.scala
@@ -25,21 +25,9 @@ class Day12Spec extends WordSpec with Matchers{
   "Cellular Automata" should {
     "evolve a ruleset for one generation" in {
 
-      val result = List("#..#.#..##......###...###",
-                        "#...#....#.....#..#..#..#")
+      //val result = "#...#....#.....#..#..#..#"
+      val result = List(0, 4, 9, 15, 18, 21, 24)
 
-      val state = CellularAutomata.evolve(1, ruleSet, initalState)
-
-      state.map(CellularAutomata.flattenToString) shouldBe result
-
-    }
-
-    "evolve a ruleset for one generation with indexes" in {
-
-      val result = List(List(('#',0),('.',1),('.',2),('#',3),('.',4),('#',5),('.',6),('.',7),('#',8),('#',9),('.',10),
-        ('.',11),('.',12),('.',13),('.',14),('.',15),('#',16),('#',17),('#',18),('.',19),('.',20),('.',21),('#',22),('#',23),('#',24)),
-        List(('#',0),('.',1),('.',2),('.',3),('#',4),('.',5),('.',6),('.',7),('.',8),('#',9),('.',10),
-          ('.',11),('.',12),('.',13),('.',14),('#',15),('.',16),('.',17),('#',18),('.',19),('.',20),('#',21),('.',22),('.',23),('#',24)))
 
       val state = CellularAutomata.evolve(1, ruleSet, initalState)
 
@@ -49,23 +37,22 @@ class Day12Spec extends WordSpec with Matchers{
 
     "extend beyond it's initial state size as approptiate" in {
 
-      val result = List(
-        "#..#.#..##......###...###",
-        "#...#....#.....#..#..#..#",
-        "##..##...##....#..#..#..##")
+      //val result = "##..##...##....#..#..#..##"
+      val result = List(0, 1,  4, 5, 9, 10, 15, 18, 21, 24, 25)
 
-      val states = CellularAutomata.evolve(2, ruleSet, initalState)
+      val state = CellularAutomata.evolve(2, ruleSet, initalState)
 
-      states.map(CellularAutomata.flattenToString) shouldBe result
+      state shouldBe result
     }
 
     "evolve correctly to 20 generations" in {
 
-      val result = "#....##....#####...#######....#.#..##"
+      //val result = "#....##....#####...#######....#.#..##"
+      val result = List(-2, 3, 4, 9, 10, 11, 12, 13, 17, 18, 19, 20, 21, 22, 23, 28, 30, 33, 34)
 
-      val state = CellularAutomata.evolve(20, ruleSet, initalState).last
+      val state = CellularAutomata.evolve(20, ruleSet, initalState)
 
-      CellularAutomata.flattenToString(state) shouldBe result
+      state shouldBe result
     }
 
     "count the plant pots after a specific generation" in {
