cb8aa254d2fad18d9ac7add25428c1de7aa909ee
daniel.manning <daniel.manning@digital.hmrc.gov.uk>
2019-09-18 13:51:11 +0100

    master: WIP Day 14

--- src/main/scala/Day14/Day14.scala
+++ src/main/scala/Day14/Day14.scala
@@ -6,12 +6,30 @@ object Day14 extends App {
 }
 
 object RecipeList {
+  def nextStep(elfState: (String, (Int, Int))):(String, (Int, Int)) = {
+    val (recipeList, (elfP1, elfP2)) = elfState
+    val newRecipeList = createNewRecipeList(recipeList, elfP1, elfP2)
+    val newElfPos = moveElves(newRecipeList, elfP1, elfP2)
+
+    (newRecipeList, newElfPos)
+  }
+
+
+  def moveElves(recipeScoreBoard: String, elfPosition1: Int, elfPosition2: Int): (Int, Int) = {
+    val positionShift1 = lookUpScore(recipeScoreBoard, elfPosition1) + 1
+    val positionShift2 = lookUpScore(recipeScoreBoard, elfPosition2) + 1
+
+    ((elfPosition1 + positionShift1) % recipeScoreBoard.length, (elfPosition2 + positionShift2) % recipeScoreBoard.length)
+  }
+
   def createNewRecipeList(recipeList:String, elfPosition1:Int, elfPosition2:Int): String = {
 
-    val score1 = recipeList.substring(elfPosition1, elfPosition1 + 1).toInt
-    val score2 = recipeList.substring(elfPosition2, elfPosition2 + 1).toInt
-
+    val score1 = lookUpScore(recipeList, elfPosition1)
+    val score2 = lookUpScore(recipeList, elfPosition2)
 
     recipeList ++ (score1 + score2).toString
   }
+
+  private def lookUpScore(recipeScoreBoard: String, position: Int):Int =
+    recipeScoreBoard.substring(position, position+1).toInt
 }
--- src/test/scala/Day14/Day14Spec.scala
+++ src/test/scala/Day14/Day14Spec.scala
@@ -7,10 +7,39 @@ class Day14Spec extends WordSpec with Matchers {
   "Generate new recipies" should {
     "create 1 new recipe when there is only 1 digit" in {
       val recipeScoreBoard = "23"
-      val elfPosition1 = 1
-      val elfPosition2 = 2
+      val elfPosition1 = 0
+      val elfPosition2 = 1
 
       RecipeList.createNewRecipeList(recipeScoreBoard, elfPosition1, elfPosition2) shouldBe "235"
+    }
+
+    "create 2 new recipes when there are 2 digits" in {
+      val recipeScoreBoard = "37"
+      val elfPosition1 = 0
+      val elfPosition2 = 1
+
+      RecipeList.createNewRecipeList(recipeScoreBoard, elfPosition1, elfPosition2) shouldBe "3710"
+    }
+  }
+
+  "Moving Elves" should {
+    "move elves by the score of their recipe" in {
+      val recipeScoreBoard = "37"
+      val elfPosition1 = 0
+      val elfPosition2 = 1
+
+      RecipeList.moveElves(recipeScoreBoard, elfPosition1, elfPosition2) shouldBe (0, 1)
+
+    }
+  }
+
+  "NextStep" should {
+    "evolve the score board and advance the elves" in {
+      val recipeScoreBoard = "3710"
+      val elfPosition1 = 0
+      val elfPosition2 = 1
+
+      RecipeList.nextStep((recipeScoreBoard, (elfPosition1, elfPosition2))) shouldBe ("371010", (4, 3))
     }
   }
 
