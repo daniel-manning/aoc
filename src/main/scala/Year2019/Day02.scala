31b6c94141b7792ce9b7fb3ff7b153551af14fdc
D <daniel.manning@digital.hmrc.gov.uk>
2019-12-03 18:24:32 +0000

    Day 02 Completed

---
+++ src/main/scala/Year2019/Day02.scala
@@ -0,0 +1,51 @@
+package Year2019
+
+object Day02 extends App {
+
+
+  def nextOperation(cursorPosition:Int, programme: IntCodeProgramme) =
+    programme.programme(cursorPosition) match {
+      case 1 => Some(Operation(programme.programme(cursorPosition + 1), programme.programme(cursorPosition + 2), programme.programme(cursorPosition + 3), _ + _))
+      case 2 => Some(Operation(programme.programme(cursorPosition + 1), programme.programme(cursorPosition + 2), programme.programme(cursorPosition + 3), _ * _))
+      case 99 => None
+    }
+
+  def performOperation(op: Option[Operation], programme: IntCodeProgramme): IntCodeProgramme =
+    op.map{ operation =>
+      IntCodeProgramme(programme.programme.updated(operation.address3,
+        operation.operator(programme.programme(operation.address1), programme.programme(operation.address2))))
+    }.getOrElse(programme)
+
+
+  def runProgramme(programme: IntCodeProgramme): IntCodeProgramme =
+  LazyList.unfold((0, programme: IntCodeProgramme)){
+    a => val op = nextOperation(a._1, a._2)
+      val nextProgramme = performOperation(op, a._2)
+
+      op.map{ _ =>
+        (nextProgramme, (a._1 + 4, nextProgramme))
+      }
+  }.last
+
+
+/*  val startingProgramme = IntCodeProgramme(Vector(1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,6,1,19,1,5,19,23,2,9,23,27,1,6,27,31,1,31,9,35,2,35,10,39,1,5,39,43,2,43,9,47,1,5,47,51,1,51,5,55,1,55,9,59,2,59,13,63,1,63,9,67,1,9,67,71,2,71,10,75,1,75,6,79,2,10,79,83,1,5,83,87,2,87,10,91,1,91,5,95,1,6,95,99,2,99,13,103,1,103,6,107,1,107,5,111,2,6,111,115,1,115,13,119,1,119,2,123,1,5,123,0,99,2,0,14,0))
+
+  val endProgramme = runProgramme(startingProgramme)
+  println(s"At the end of the programme at position 0 the value is ${endProgramme.programme(0)}")*/
+
+  def startingProgramme(a:Int, b:Int) = IntCodeProgramme(Vector(1,a,b,3,1,1,2,3,1,3,4,3,1,5,0,3,2,6,1,19,1,5,19,23,2,9,23,27,1,6,27,31,1,31,9,35,2,35,10,39,1,5,39,43,2,43,9,47,1,5,47,51,1,51,5,55,1,55,9,59,2,59,13,63,1,63,9,67,1,9,67,71,2,71,10,75,1,75,6,79,2,10,79,83,1,5,83,87,2,87,10,91,1,91,5,95,1,6,95,99,2,99,13,103,1,103,6,107,1,107,5,111,2,6,111,115,1,115,13,119,1,119,2,123,1,5,123,0,99,2,0,14,0))
+
+  val params = for{
+    a <- (0 to 100)
+    b <- (0 to 100)
+    if(runProgramme(startingProgramme(a,b)).programme(0) == 19690720)
+  } yield (a, b)
+
+  println(s"The combination leading to the moon landing is: $params")
+  params.map(a => println(s"Noun: ${a._1} Verb: ${a._2}  So value is ${100*a._1 + a._2}"))
+
+}
+
+
+case class Operation(address1: Int, address2:Int, address3: Int, operator: (Int, Int) => Int)
+case class IntCodeProgramme(programme:Vector[Int])
---
+++ src/test/scala/Year2019/Day02Spec.scala
@@ -0,0 +1,71 @@
+package Year2019
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class Day02Spec extends AnyWordSpec with Matchers{
+
+
+  "A programme" should {
+    "edit in place it's code values" in {
+      val startingProgramme = IntCodeProgramme(Vector(1,9,10,3,2,3,11,0,99,30,40,50))
+      val op = Day02.nextOperation(0, startingProgramme)
+      val alteredProgramme = Day02.performOperation(op, startingProgramme)
+      alteredProgramme shouldBe IntCodeProgramme(Vector(1,9,10,70,2,3,11,0,99,30,40,50))
+    }
+
+    "start at a separate starting place for the next opcode" in {
+      val startingProgramme = IntCodeProgramme(Vector(1,9,10,70,2,3,11,0,99,30,40,50))
+      val op = Day02.nextOperation(4, startingProgramme)
+      val alteredProgramme = Day02.performOperation(op, startingProgramme)
+      alteredProgramme shouldBe IntCodeProgramme(Vector(3500,9,10,70,2,3,11,0,99,30,40,50))
+    }
+
+    "end when it hits the end opcode" in {
+      val startingProgramme = IntCodeProgramme(Vector(3500,9,10,70,2,3,11,0,99,30,40,50))
+      val op = Day02.nextOperation(8, startingProgramme)
+      val alteredProgramme = Day02.performOperation(op, startingProgramme)
+      alteredProgramme shouldBe IntCodeProgramme(Vector(3500,9,10,70,2,3,11,0,99,30,40,50))
+    }
+  }
+
+  "Running a programme" should {
+    "run correctly through to the end" in {
+      val startingProgramme = IntCodeProgramme(Vector(1,9,10,3,2,3,11,0,99,30,40,50))
+      val alteredProgramme = Day02.runProgramme(startingProgramme)
+
+      alteredProgramme shouldBe IntCodeProgramme(Vector(3500,9,10,70,2,3,11,0,99,30,40,50))
+    }
+
+    "run an addition correctly" in {
+      val startingProgramme = IntCodeProgramme(Vector(1,0,0,0,99))
+      val alteredProgramme = Day02.runProgramme(startingProgramme)
+
+      alteredProgramme shouldBe IntCodeProgramme(Vector(2,0,0,0,99))
+    }
+
+    "run a multiplication correctly" in {
+      val startingProgramme = IntCodeProgramme(Vector(2,3,0,3,99))
+      val alteredProgramme = Day02.runProgramme(startingProgramme)
+
+      alteredProgramme shouldBe IntCodeProgramme(Vector(2,3,0,6,99))
+    }
+
+    "run a multiplication on an opcode" in {
+      val startingProgramme = IntCodeProgramme(Vector(2,4,4,5,99,0))
+      val alteredProgramme = Day02.runProgramme(startingProgramme)
+
+      alteredProgramme shouldBe IntCodeProgramme(Vector(2,4,4,5,99,9801))
+    }
+
+    "run multiple operations correctly" in {
+      val startingProgramme = IntCodeProgramme(Vector(1,1,1,4,99,5,6,0,99))
+      val alteredProgramme = Day02.runProgramme(startingProgramme)
+
+      alteredProgramme shouldBe IntCodeProgramme(Vector(30,1,1,4,2,5,6,0,99))
+    }
+  }
+
+
+
+}
