2768c391b648b6bfd4f793fdccded0fc0579a7a8
D <daniel.manning@digital.hmrc.gov.uk>
2019-12-12 22:52:50 +0000

    Day 10 part 1 complete

--- src/main/scala/Year2019/Day10.scala
+++ src/main/scala/Year2019/Day10.scala
@@ -1,6 +1,18 @@
 package Year2019
 
+import scala.io.Source
+
 object Day10 extends App {
+  val asteroidSeq: Seq[String] = Source.fromResource("2019/day10")
+    .getLines()
+    .toList
+
+
+  val asteroidField = AsteroidField.construct(asteroidSeq)
+
+  val (bestLocation, asteroidsCovered) = asteroidField.findBestPositionForAsteroidBase
+
+  println(s"The best position in the asteroid field is $bestLocation where it observes $asteroidsCovered asteroids")
 
 }
 
@@ -16,12 +28,91 @@ object Space {
     }
 }
 
-case class AsteroidField(field: Map[Position, Space])
+case class Rational(num: Int, denom: Int){
+  def isInteger: Boolean = denom == 1
+
+  def lowestTerms: Rational = {
+    val multiplier = denom.sign.toInt
+    val gcd = BigInt(num).gcd(denom).toInt
+
+    Rational(multiplier * num / gcd, multiplier * denom / gcd)
+  }
+
+  def +(that: Rational): Rational =
+    Rational(this.num*that.denom + that.num*this.denom, this.denom*that.denom).lowestTerms
+
+  def *(that: Rational): Rational =
+    Rational(this.num*that.num, this.denom*that.denom).lowestTerms
+
+  def floor: Int = Math.floor(num.toDouble / denom).toInt
+}
+
+object Rational {
+  implicit def fromInt(value: Int): Rational = Rational(value, 1)
+}
+
+case class AsteroidField(field: Map[Position, Space]){
+
+  def noOfAsteroidsVisibleFromPosition(base: Position): Int = {
+      field.count { a =>
+        val asteroidsInLineBefore =
+          if (base == a._1) {
+            1
+          } else if (base.x == a._1.x) {
+            val yRange = if (base.y > a._1.y) {
+              ((a._1.y + 1) until base.y)
+            } else {
+              ((base.y + 1) until a._1.y)
+            }
+
+            yRange.count(y => field.contains(Position(base.x, y)))
+          } else if (base.y == a._1.y) {
+            val xRange: Range = if (base.x > a._1.x) {
+              ((a._1.x + 1) until base.x)
+            } else {
+              ((base.x + 1) until a._1.x)
+            }
+
+            xRange.count(x => field.contains(Position(x, base.y)))
+          } else {
+            val gradient = Rational(base.y - a._1.y, base.x - a._1.x)
+
+            val xRange: Range = if (base.x > a._1.x) {
+              ((a._1.x + 1) until base.x)
+            } else {
+              ((base.x + 1) until a._1.x)
+            }
+
+            xRange.count { x =>
+              val y = gradient * (x - base.x) + base.y
+
+              if (y.isInteger) {
+                field.contains(Position(x, y.num))
+              } else false
+            }
+          }
+
+        asteroidsInLineBefore == 0
+      }
+  }
+
+  def scorePositionsForAsteroidBase: Seq[(Position, Int)] =
+    field.toSeq.sortBy(l => (l._1.x, l._1.y)).map(p => (p._1, noOfAsteroidsVisibleFromPosition(p._1)))
+
+
+  def findBestPositionForAsteroidBase: (Position, Int) =
+    scorePositionsForAsteroidBase.maxBy(_._2)
+}
 
 object AsteroidField {
   def construct(field: Seq[String]): AsteroidField = {
-   val asteroidField = ArraySupport.sequenceArrayToPositionSeq(field.map(_.toSeq))(Space.fromChar).toMap
-    //println(asteroidField)
+   val asteroidField = ArraySupport.sequenceArrayToPositionSeq(field.map(_.toSeq))(Space.fromChar).filter {
+     sp => sp._2 match {
+       case Asteroid => true
+       case _ => false
+     }
+   }.toMap
+
     AsteroidField(asteroidField)
   }
 }
--- src/test/scala/Year2019/Day10Spec.scala
+++ src/test/scala/Year2019/Day10Spec.scala
@@ -5,23 +5,133 @@ import org.scalatest.wordspec.AnyWordSpec
 
 class Day10Spec extends AnyWordSpec with Matchers {
 
+  val fieldString =
+    """.#..#
+      |.....
+      |#####
+      |....#
+      |...##""".stripMargin
+
   "AsteroidField" should {
     "construct from String" in {
-      val fieldString =
-        """.#..#
-          |.....
-          |#####
-          |....#
-          |...##""".stripMargin
       val result = Map(
-        Position(0,0) -> Empty, Position(1,0) -> Asteroid, Position(2,0) -> Empty, Position(3,0) -> Empty, Position(4,0) -> Asteroid,
-        Position(0,1) -> Empty, Position(1,1) -> Empty, Position(2,1) -> Empty, Position(3,1) -> Empty, Position(4,1) -> Empty,
+        Position(1,0) -> Asteroid, Position(4,0) -> Asteroid,
         Position(0,2) -> Asteroid, Position(1,2) -> Asteroid, Position(2,2) -> Asteroid, Position(3,2) -> Asteroid, Position(4,2) -> Asteroid,
-        Position(0,3) -> Empty, Position(1,3) -> Empty, Position(2,3) -> Empty, Position(3,3) -> Empty, Position(4,3) -> Asteroid,
-        Position(0,4) -> Empty, Position(1,4) -> Empty, Position(2,4) -> Empty, Position(3,4) -> Asteroid, Position(4,4) -> Asteroid
+        Position(4,3) -> Asteroid,
+        Position(3,4) -> Asteroid, Position(4,4) -> Asteroid
       )
 
       AsteroidField.construct(fieldString.split("\n")) shouldBe AsteroidField(result)
+    }
+
+    "be able to calculate the asteroids visible from" in {
+      val field = AsteroidField.construct(fieldString.split("\n"))
+      field.noOfAsteroidsVisibleFromPosition(Position(3,4)) shouldBe 8
+    }
+
+    "be able to correctly score positions for asteroid base" in {
+      val result = Seq((Position(0,2),6), (Position(1,0),7), (Position(1,2),7), (Position(2,2),7), (Position(3,2),7),
+        (Position(3,4),8), (Position(4,0),7), (Position(4,2),5), (Position(4,3),7), (Position(4,4),7))
+      val field = AsteroidField.construct(fieldString.split("\n"))
+      field.scorePositionsForAsteroidBase shouldBe result
+    }
+
+    "be able to find the best position for asteroid base" in {
+      val field = AsteroidField.construct(fieldString.split("\n"))
+      field.findBestPositionForAsteroidBase shouldBe (Position(3,4), 8)
+    }
+
+    "be able to match test case one" in {
+      val testField =
+          """......#.#.
+            |#..#.#....
+            |..#######.
+            |.#.#.###..
+            |.#..#.....
+            |..#....#.#
+            |#..#....#.
+            |.##.#..###
+            |##...#..#.
+            |.#....####""".stripMargin
+
+      val field = AsteroidField.construct(testField.split("\n"))
+      field.findBestPositionForAsteroidBase shouldBe (Position(5,8), 33)
+    }
+
+    "be able to match test case two" in {
+      val testField =
+        """#.#...#.#.
+          |.###....#.
+          |.#....#...
+          |##.#.#.#.#
+          |....#.#.#.
+          |.##..###.#
+          |..#...##..
+          |..##....##
+          |......#...
+          |.####.###.""".stripMargin
+
+      val field = AsteroidField.construct(testField.split("\n"))
+      field.findBestPositionForAsteroidBase shouldBe (Position(1,2), 35)
+    }
+
+    "be able to match test case three" in {
+      val testField =
+        """.#..#..###
+          |####.###.#
+          |....###.#.
+          |..###.##.#
+          |##.##.#.#.
+          |....###..#
+          |..#.#..#.#
+          |#..#.#.###
+          |.##...##.#
+          |.....#.#..""".stripMargin
+
+      val field = AsteroidField.construct(testField.split("\n"))
+      field.findBestPositionForAsteroidBase shouldBe (Position(6,3), 41)
+    }
+
+    "be able to match test case four" in {
+      val testField =
+        """.#..##.###...#######
+          |##.############..##.
+          |.#.######.########.#
+          |.###.#######.####.#.
+          |#####.##.#.##.###.##
+          |..#####..#.#########
+          |####################
+          |#.####....###.#.#.##
+          |##.#################
+          |#####.##.###..####..
+          |..######..##.#######
+          |####.##.####...##..#
+          |.#####..#.######.###
+          |##...#.##########...
+          |#.##########.#######
+          |.####.#.###.###.#.##
+          |....##.##.###..#####
+          |.#.#.###########.###
+          |#.#.#.#####.####.###
+          |###.##.####.##.#..##""".stripMargin
+
+      val field = AsteroidField.construct(testField.split("\n"))
+      field.findBestPositionForAsteroidBase shouldBe (Position(11,13), 210)
+    }
+  }
+
+
+  "Rational" should {
+    "reduction fractions to their lowest term" in {
+      Rational(4, 12).lowestTerms shouldBe Rational(1, 3)
+    }
+
+    "add two rationals" in {
+      Rational(1, 2) + Rational(1, 4) shouldBe Rational(3, 4)
+    }
+
+    "multiply two rationals" in {
+      Rational(1, 2) * Rational(1, 4) shouldBe Rational(1, 8)
     }
   }
 }
