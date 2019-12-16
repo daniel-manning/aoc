4b24ca618d5bd50088248aae4dc3e7bffdcec460
daniel.manning <daniel.manning@digital.hmrc.gov.uk>
2019-12-16 17:29:46 +0000

    master: Implement pair crossing

--- src/main/scala/Year2019/SeqOps.scala
+++ src/main/scala/Year2019/SeqOps.scala
@@ -2,7 +2,15 @@ package Year2019
 
 object SeqOps {
 
-  def crossDifferentPairs[A](sequence: Seq[A]): Seq[(A,A)] = ???
+  def crossDifferentPairs[A](sequence: Seq[A]): Seq[(A,A)] = {
+    @scala.annotation.tailrec
+    def go(result: Seq[(A,A)], rest: Seq[A]): Seq[(A,A)] = {
+      if (rest.isEmpty) result
+      else go(result ++ rest.tail.map(a => (rest.head, a)), rest.tail)
+    }
+
+    go(Seq.empty, sequence)
+  }
 
 
 }
--- src/test/scala/Year2019/SeqOpsSupport.scala
+++ src/test/scala/Year2019/SeqOpsSupport.scala
@@ -7,7 +7,7 @@ class SeqOpsSupport extends AnyWordSpec with Matchers {
 
   "Sequences" should {
     "be able to cross with different pairs" in {
-      val result = Seq((1,2), (1, 3), (2, 1), (2, 3))
+      val result = Seq((1,2), (1, 3), (2, 3))
       SeqOps.crossDifferentPairs(Seq(1,2,3)) shouldBe result
     }
   }
