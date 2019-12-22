9da8135c513661be56c7d3e1058a0e393470ff16
D <daniel.manning@digital.hmrc.gov.uk>
2019-12-22 17:39:53 +0000

    Day 14 part 2 wip

--- src/main/scala/Year2019/Day14.scala
+++ src/main/scala/Year2019/Day14.scala
@@ -13,10 +13,13 @@ object Day14 extends App {
   val oreIngredient = CriticalPath.minimumInput(recipe)
 
   println(s"To create 1 unit of FUEL we need ${oreIngredient.volume} units of ORE")
+
+  val maxFuelForOre = CriticalPath.calculateYieldForFuel(recipe, Ingredient(BigInt("1000000000000"), "ORE"))
+  println(s"Maximum fuel for ore: $maxFuelForOre")
 }
 
 
-case class Ingredient(volume: Int, name: String)
+case class Ingredient(volume: BigInt, name: String)
 
 case class Reaction(requirements: Seq[Ingredient], result: Ingredient)
 
@@ -36,16 +39,36 @@ object ReactionParser extends RegexParsers {
 
 
 object CriticalPath {
+  def calculateYieldForFuel(reactions: Seq[Reaction], oreAmount: Ingredient): BigInt = {
+    val orePerFuel = minimumInput(reactions)
+    val firstApproximation = oreAmount.volume / orePerFuel.volume
+    val exponent = firstApproximation.toString.length
+
+       @scala.annotation.tailrec
+       def iterateUntilOverEstimate(value: BigInt, step:BigInt, limit: BigInt): BigInt = {
+         val total = minimumInput(reactions, value).volume
+         println(s"value: $value, step: $step, limit: $limit, total: $total")
+         if(total > limit){
+           if(step == BigInt(1)) value - step
+           else iterateUntilOverEstimate(value - step, (step/BigInt(10)), limit)
+         } else {
+           iterateUntilOverEstimate(value + step, step, limit)
+         }
+       }
+
+    iterateUntilOverEstimate(firstApproximation, BigInt(10).pow(exponent - 1), oreAmount.volume)
+  }
+
   def makeReactionListFromRecipe(recipe: String): Seq[Reaction] =
     recipe.split("\n").map{ s =>
       ReactionParser.parse(ReactionParser.reaction, s).get
     }.toSeq
 
-  def minimumInput(reactions: Seq[Reaction]): Ingredient = {
+  def minimumInput(reactions: Seq[Reaction], forFuel: BigInt = BigInt(1)): Ingredient = {
 
    val reactionMap = turnReactionsToMap(reactions)
 
-    val ingredients = Seq(Ingredient(1, "FUEL"))
+    val ingredients = Seq(Ingredient(forFuel, "FUEL"))
 
     @scala.annotation.tailrec
     def go(reactionGraph: Map[String, Seq[String]], ingredientList: Seq[Ingredient]): Seq[Ingredient] = {
@@ -59,6 +82,62 @@ object CriticalPath {
     go(reactionMap, ingredients).find(_.name == "ORE").get
   }
 
+  def results(reactions: Seq[Reaction]): Set[Ingredient] = {
+    val ore = minimumInput(reactions)
+    val ingredient = Set(ore)
+
+    @scala.annotation.tailrec
+    def go(reactions: Seq[Reaction], ingredients: Set[Ingredient]):Set[Ingredient] = {
+      val all = findEachAvailableReaction(reactions, ingredients)
+      println(s"all: $all")
+      val updatedIngredients = useRecipe(all, ingredients)
+      println(s"updatedIngredients: $updatedIngredients")
+
+      if(updatedIngredients.exists(_.name == "FUEL")) updatedIngredients
+      else go(reactions, updatedIngredients)
+    }
+
+    go(reactions, ingredient)
+  }
+
+
+  def useRecipe(reactions: Seq[Reaction], ingredients: Set[Ingredient]): Set[Ingredient] =
+    reactions.foldRight(ingredients)((a, b) =>  useRecipe(a, reactions, b))
+
+  def useRecipe(reaction: Reaction, reactions: Seq[Reaction], ingredients: Set[Ingredient]): Set[Ingredient] = {
+    println(s"Trying to run reaction: $reaction")
+    val list = reaction.requirements.foldLeft((ingredients.toSet)) {
+      (a, b) =>
+        println(s"a: $a")
+        println(s"b: $b")
+        val ingredient = a.find(_.name == b.name).get
+        val removed = a.removedAll(Set(ingredient))
+        if (ingredient.volume == b.volume)
+          removed
+        else if (ingredient.volume < b.volume) {
+          //we need more reaction ingredients to satisfy current reactions
+          val reactionIngredientToBoost = reactions.find(_.result.name == ingredient.name).get
+          val newIngredients = useRecipe(reactionIngredientToBoost, reactions, a)
+          println(s"newIngredients: $newIngredients")
+          val newIngredient = newIngredients.find(_.name == b.name).get
+
+          val newPot = newIngredients.removedAll(Set(newIngredient)).union(Set(Ingredient(newIngredient.volume - b.volume, b.name)))
+          println(s"New reacted pot: $newPot")
+          newPot
+        }
+        else
+          removed.union(Set(Ingredient(ingredient.volume - b.volume, b.name)))
+    }
+
+    //add reaction to ingredients adding in new
+    list.find(_.name == reaction.result.name)
+      .map(i => list.removedAll(Set(i)).union(Set(Ingredient(reaction.result.volume + i.volume, i.name))))
+      .getOrElse(list.union(Set(reaction.result)))
+  }
+
+  def findEachAvailableReaction(reactions:Seq[Reaction], ingredients: Set[Ingredient]): Seq[Reaction] =
+    reactions.filter(r => /*!ingredients.exists(_.name == r.result.name) &&*/ r.requirements.forall(i => ingredients.exists(_.name == i.name)))
+
   def decomposeTarget(name:String, ingredients: Seq[Ingredient], reactions: Seq[Reaction]): Seq[Ingredient] = {
     val targetReaction = reactions.find(_.result.name == name).get
 
@@ -71,7 +150,7 @@ object CriticalPath {
     val totalReactants = if(requiredVolume <= targetVolume){
       requirements
     } else {
-      val scalefactor = Math.ceil(requiredVolume.toDouble/targetVolume).toInt
+      val scalefactor = Math.ceil((BigDecimal(requiredVolume)/BigDecimal(targetVolume)).toDouble).toInt
       requirements.map(i => Ingredient(i.volume*scalefactor, i.name))
     }
 
@@ -87,7 +166,7 @@ object CriticalPath {
   def breadthFirstSearch(graph: Map[String, Seq[String]], ingredients: Seq[Ingredient], reactions: Seq[Reaction]): (Seq[Ingredient], Map[String, Seq[String]]) = {
     val nextSearch = findEnds(graph)
 
-    println(s"removing ends: $nextSearch")
+    //println(s"removing ends: $nextSearch")
     nextSearch.foldLeft((ingredients, graph)) {
       (newIngredients, name) =>
         (decomposeTarget(name, newIngredients._1, reactions), newIngredients._2 - name)
--- src/test/scala/Year2019/Day14Spec.scala
+++ src/test/scala/Year2019/Day14Spec.scala
@@ -181,4 +181,138 @@ class Day14Spec extends AnyWordSpec with Matchers {
     }
   }
 
+  "Find maximum yield" should {
+    "calculate yield for test case one" in {
+      val reactionInput =
+        """157 ORE => 5 NZVS
+          |165 ORE => 6 DCFZ
+          |44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL
+          |12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ
+          |179 ORE => 7 PSHF
+          |177 ORE => 5 HKGWZ
+          |7 DCFZ, 7 PSHF => 2 XJWVT
+          |165 ORE => 2 GPVTF
+          |3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT""".stripMargin
+
+      val reactions = CriticalPath.makeReactionListFromRecipe(reactionInput)
+      val maximumOre = Ingredient(BigInt("1000000000000"), "ORE")
+
+      CriticalPath.calculateYieldForFuel(reactions, maximumOre) shouldBe 82892753
+    }
+
+    "calculate yield for test case two" in {
+      val reactionInput =
+        """2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG
+          |17 NVRVD, 3 JNWZP => 8 VPVL
+          |53 STKFG, 6 MNCFX, 46 VJHF, 81 HVMC, 68 CXFTF, 25 GNMV => 1 FUEL
+          |22 VJHF, 37 MNCFX => 5 FWMGM
+          |139 ORE => 4 NVRVD
+          |144 ORE => 7 JNWZP
+          |5 MNCFX, 7 RFSQX, 2 FWMGM, 2 VPVL, 19 CXFTF => 3 HVMC
+          |5 VJHF, 7 MNCFX, 9 VPVL, 37 CXFTF => 6 GNMV
+          |145 ORE => 6 MNCFX
+          |1 NVRVD => 8 CXFTF
+          |1 VJHF, 6 MNCFX => 4 RFSQX
+          |176 ORE => 6 VJHF""".stripMargin
+
+      val reactions = CriticalPath.makeReactionListFromRecipe(reactionInput)
+      val maximumOre = Ingredient(BigInt("1000000000000"), "ORE")
+
+      CriticalPath.calculateYieldForFuel(reactions, maximumOre) shouldBe 5586022
+    }
+
+    "calculate yield for test case three" in {
+      val reactionInput =
+        """171 ORE => 8 CNZTR
+          |7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL
+          |114 ORE => 4 BHXH
+          |14 VRPVC => 6 BMBT
+          |6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL
+          |6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT
+          |15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW
+          |13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW
+          |5 BMBT => 4 WPTQ
+          |189 ORE => 9 KTJDG
+          |1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP
+          |12 VRPVC, 27 CNZTR => 2 XDBXC
+          |15 KTJDG, 12 BHXH => 5 XCVML
+          |3 BHXH, 2 VRPVC => 7 MZWV
+          |121 ORE => 7 VRPVC
+          |7 XCVML => 6 RJRHP
+          |5 BHXH, 4 VRPVC => 5 LTCX""".stripMargin
+
+      val reactions = CriticalPath.makeReactionListFromRecipe(reactionInput)
+      val maximumOre = Ingredient(BigInt("1000000000000"), "ORE")
+
+      CriticalPath.calculateYieldForFuel(reactions, maximumOre) shouldBe 460664
+    }
+
+    "use a recipe to create an ingredient" in  {
+      val reactionInput =
+        """171 ORE => 8 CNZTR
+          |114 ORE => 4 BHXH
+          |189 ORE => 9 KTJDG""".stripMargin
+
+      val reactions = CriticalPath.makeReactionListFromRecipe(reactionInput)
+      val ore = Ingredient(BigInt("475"), "ORE")
+
+      CriticalPath.useRecipe(reactions, Set(ore)) shouldBe Set(Ingredient(BigInt(1), "ORE"), Ingredient(BigInt(8), "CNZTR"),
+        Ingredient(BigInt(4), "BHXH"), Ingredient(BigInt(9), "KTJDG"))
+    }
+
+    "find the results from the minimum ore per fuel" in  {
+      val reactionInput =
+        """171 ORE => 8 CNZTR
+          |7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL
+          |114 ORE => 4 BHXH
+          |14 VRPVC => 6 BMBT
+          |6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL
+          |6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT
+          |15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW
+          |13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW
+          |5 BMBT => 4 WPTQ
+          |189 ORE => 9 KTJDG
+          |1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP
+          |12 VRPVC, 27 CNZTR => 2 XDBXC
+          |15 KTJDG, 12 BHXH => 5 XCVML
+          |3 BHXH, 2 VRPVC => 7 MZWV
+          |121 ORE => 7 VRPVC
+          |7 XCVML => 6 RJRHP
+          |5 BHXH, 4 VRPVC => 5 LTCX""".stripMargin
+
+      val reactions = CriticalPath.makeReactionListFromRecipe(reactionInput)
+
+      CriticalPath.results(reactions) shouldBe Set(Ingredient(BigInt(1), "FUEL"))
+    }
+
+    "run previous reactions when it needs extra ingredients" in  {
+      val reactionInput =
+        """171 ORE => 8 CNZTR
+          |7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL
+          |114 ORE => 4 BHXH
+          |14 VRPVC => 6 BMBT
+          |6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL
+          |6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT
+          |15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW
+          |13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW
+          |5 BMBT => 4 WPTQ
+          |189 ORE => 9 KTJDG
+          |1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP
+          |12 VRPVC, 27 CNZTR => 2 XDBXC
+          |15 KTJDG, 12 BHXH => 5 XCVML
+          |3 BHXH, 2 VRPVC => 7 MZWV
+          |121 ORE => 7 VRPVC
+          |7 XCVML => 6 RJRHP
+          |5 BHXH, 4 VRPVC => 5 LTCX""".stripMargin
+
+      val reactions = CriticalPath.makeReactionListFromRecipe(reactionInput)
+
+      val ingredients = Set(Ingredient(7,"VRPVC"), Ingredient(8,"CNZTR"), Ingredient(4,"BHXH"), Ingredient(9,"KTJDG"), Ingredient(2210141,"ORE"))
+      val reaction = Reaction(List(Ingredient(5,"BHXH"), Ingredient(4,"VRPVC")),Ingredient(5,"LTCX"))
+
+      CriticalPath.useRecipe(reaction, reactions, ingredients) shouldBe
+        Set(Ingredient(3,"VRPVC"), Ingredient(8,"CNZTR"), Ingredient(3,"BHXH"), Ingredient(9,"KTJDG"), Ingredient(5,"LTCX"), Ingredient(2210027,"ORE"))
+    }
+  }
+
 }
