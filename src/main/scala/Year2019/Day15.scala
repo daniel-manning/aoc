e205c1c808343d69221a9315d0597c8286c1ba9a
D <daniel.manning@digital.hmrc.gov.uk>
2019-12-26 01:56:38 +0000

    Day 15 part 1 wip

--- src/main/scala/Year2019/Day15.scala
+++ src/main/scala/Year2019/Day15.scala
@@ -1,14 +1,18 @@
 package Year2019
 
+import java.awt.Color
+
 import Year2019.ProgrammeOperations.vectorProgrammeToMap
+import monix.reactive.Observable
+import monix.execution.Scheduler.Implicits.global
 
 import scala.collection.mutable
 import scala.concurrent.Future
 import scala.io.Source
+import scala.concurrent.duration.{Duration, MILLISECONDS}
+import scala.swing.{Dimension, Graphics2D, MainFrame, Panel, SimpleSwingApplication}
 
-import scala.concurrent.ExecutionContext.Implicits.global
-
-object Day15 {
+object Day15 extends SimpleSwingApplication {
 
   val sourceCode: Vector[BigInt] = Source.fromResource("2019/day15")
     .getLines()
@@ -20,7 +24,60 @@ object Day15 {
 
   implicit val (ex1, queues) = RepairDroid.loadProgramme(sourceCode)
 
+   val tiles: Seq[CraftTile] = Seq()
 
+  lazy val ui: Panel = new Panel {
+    background = Color.black
+    preferredSize = new Dimension(700, 500)
+
+    focusable = true
+
+
+    override def paintComponent(g: Graphics2D): Unit = {
+      super.paintComponent(g)
+      g.setColor(Color.black)
+
+      val offsetX = 100
+      val offsetY = 250
+
+      val width: Int = 10
+      val height: Int = 10
+
+      val drawWidth: Int = 9
+      val drawHeight: Int = 9
+
+      tiles.foreach {
+        case EmptySpace(Position(x, y)) =>
+          g.setColor(Color.lightGray)
+          g.fillRect(offsetX + x * width, offsetY - y * height, drawWidth, drawHeight)
+        case CraftWall(Position(x, y)) =>
+          g.setColor(Color.white)
+          g.fillRect(offsetX + x * width, offsetY - y * height, drawWidth, drawHeight)
+        case OxygenUnit(Position(x, y)) =>
+          g.setColor(Color.yellow)
+          g.fillRect(offsetX + x * width, offsetY - y * height, drawWidth, drawHeight)
+      }
+
+    }
+  }
+
+  def top: MainFrame = new MainFrame {
+    title = "RepairDroid"
+    contents = ui
+  }
+
+  def repaint(): Unit = {
+        ???
+      ui.repaint()
+  }
+
+  ///////////////MONIX TASKS
+  val tick = {
+    Observable.interval(Duration(400, MILLISECONDS))
+      .map(x => repaint())
+  }
+
+  val cancelable = tick.subscribe()
 
 }
 
@@ -28,6 +85,13 @@ sealed trait DroidStatus
 case object BlockedByWall extends DroidStatus
 case object Moved extends DroidStatus
 case object MovedToOxygenUnit extends DroidStatus
+
+sealed trait CraftTile {
+  val position: Position
+}
+case class CraftWall(position: Position) extends CraftTile
+case class EmptySpace(position: Position) extends CraftTile
+case class OxygenUnit(position: Position) extends CraftTile
 
 
 object RepairDroid {
