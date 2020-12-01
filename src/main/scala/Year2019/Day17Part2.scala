f8391bfbde89e717727e4d347530114be10c9e15
D <daniel.manning@digital.hmrc.gov.uk>
2020-12-01 00:35:33 +0000

    Day17pt2 WIP

--- src/main/scala/Year2019/Day17Part2.scala
+++ src/main/scala/Year2019/Day17Part2.scala
@@ -1,8 +1,11 @@
 package Year2019
 
+import Year2019.ProgrammeOperations.vectorProgrammeToMap
 import models.Position
 
 import scala.annotation.tailrec
+import scala.collection.mutable
+import scala.io.Source
 
 object Day17Part2Test extends App {
 val map = """#######...#####
@@ -90,7 +93,38 @@ object Day17Part2 extends App {
   println(s"*************** calculatedRoute: $path")
   val record = Record.fromPath(path)
   val instructions = record.toInstructions
-  println(instructions)
+  println(s"***************** instructions: $instructions")
+ /////////////
+ val sourceCode = Source.fromResource("2019/day17_2")
+   .getLines()
+   .toList
+   .head
+   .split(",")
+   .toVector
+   .map(n => BigInt(n.toInt))
+
+  val inputQueue = new mutable.Queue[BigInt]()
+  val outputQueue = new mutable.Queue[BigInt]()
+
+
+  val startProgramme = IntCodeProgramme(programme = vectorProgrammeToMap(sourceCode),
+      inputQueue = inputQueue,
+      outputQueue = outputQueue)
+
+
+  val endProgramme = startProgramme.runProgramme()(RunningSettings("trial", debugOutput = false))
+
+  //add programme for dust sweeping
+  inputQueue.enqueueAll(instructions.map(n => BigInt(n)))
+
+  val outputs = endProgramme.outputQueue.dequeueAll(_ => true)
+
+  val outputAscii = outputs.map(_.intValue.toChar)
+
+  println(s"---------------------------")
+  println(outputAscii)
+  println(s"---------------------------")
+
 }
 
 case class Sweeper(orientation: Orientation, location: Position)
@@ -117,7 +151,7 @@ object Sweeper {
     else {
       //find connecting point
       val possibleMoves = scaffolding.map `intersect` neighbours.map(x => sweeper.location + x).toSet
-      println(s"*********** possible moves: $possibleMoves")
+      //println(s"*********** possible moves: $possibleMoves")
       val position  = if(possibleMoves.size == 1) possibleMoves.head else throw new Exception("There is not one unique move!")
       //turn to the right direction
       val newDirection = neighbours.map(x => (x, sweeper.location + x)).filter(l => l._2 == position).head._1
@@ -125,7 +159,7 @@ object Sweeper {
       val turnDirection = Orientation.turning(sweeper.orientation, newOrientation)
       // move as far as possible
       val (noOfMoves, endPosition, leftOverScaffolding) = move(newDirection, sweeper.location, 0, scaffolding)
-      println(s"********************** move part: ${(turnDirection, noOfMoves)}")
+      //println(s"********************** move part: ${(turnDirection, noOfMoves)}")
       calculateRoute(Sweeper(newOrientation, endPosition),leftOverScaffolding, moves :+ (turnDirection, noOfMoves))
     }
   }
@@ -147,36 +181,64 @@ object Record {
     //find the longest substring repeated at least once
     //21 (from the memory limit) and then down 4 each time (I think that turning and moving is a unit)
     @tailrec
-    def go(length: Int, matchPath: String, matchString: String): String = {
-        if(matchPath.drop(length).contains(matchString)) matchString
-        else if(length == 0) ""
-        else go(length - 4, matchPath, matchString.take(length - 4))
+    def go(noOfTokens: Int, matchPath: Seq[String], matchString: Seq[String]): String = {
+        if(matchPath.drop(noOfTokens).containsSlice(matchString)) matchString.mkString(",")
+        else if(noOfTokens == 0) ""
+        else go(noOfTokens - 2, matchPath, matchString.take(noOfTokens - 2))
       }
 
-
-    val A = go( 19, path, path.take(19))
-    val newPath = path.drop(A.length + 1)
-    val B = go( 19, newPath, newPath.take(19))
-    val lastPath = newPath.drop(B.length + 1)
-    val C = go(19, lastPath, lastPath.take(19))
-    println(s"*************** A : $A")
-    println(s"***************** New Path: $newPath")
-    println(s"*************** B : $B")
-    println(s"*************** C : $C")
-
-
     @tailrec
-    def contractPath(uncontracted: String, pathParts: Seq[Char]): Seq[Char] = {
-      if(uncontracted.length == 0) pathParts
-      else if (uncontracted.startsWith(A)) contractPath(uncontracted.drop(A.length + 1), pathParts :+ 'A')
-      else if (uncontracted.startsWith(B)) contractPath(uncontracted.drop(B.length + 1), pathParts :+ 'B')
-      else if (uncontracted.startsWith(C)) contractPath(uncontracted.drop(C.length + 1), pathParts :+ 'C')
-      else throw new Exception("Something went wrong!")
+    def mopUp[A](a: Seq[A], b: Seq[A], tokenPath: Seq[A], c: Seq[A] = Seq.empty): Seq[A] = {
+      //println(s"** a: $a, b: $b, c: $c, token: $tokenPath")
+      if(tokenPath.isEmpty) c
+      else if(tokenPath.startsWith(a)) {
+        if(c.nonEmpty) c
+        else mopUp(a, b, tokenPath.drop(a.length), c)
+      } else if(tokenPath.startsWith(b)) {
+        if(c.nonEmpty) c
+        else mopUp(a, b, tokenPath.drop(b.length), c)
+      } else mopUp(a, b, tokenPath.drop(1), c :+ tokenPath.head)
     }
 
-    val reducedPath = contractPath(path, Seq.empty).mkString(",")
-    println(s"****************** path: $reducedPath")
+    @tailrec
+    def reducePath[A](a: Seq[A], b: Seq[A], c: Seq[A], tokenPath: Seq[A], route: Seq[String] = Seq.empty): Option[Seq[String]] = {
+      if(tokenPath.isEmpty) Some(route)
+      else if(tokenPath.startsWith(a)) {
+        reducePath(a, b, c, tokenPath.drop(a.length), route :+ "A")
+      } else if(tokenPath.startsWith(b)) {
+        reducePath(a, b, c, tokenPath.drop(b.length), route :+ "B")
+      } else if(tokenPath.startsWith(c)) {
+        reducePath(a, b, c, tokenPath.drop(c.length), route :+ "C")
+      } else None
+    }
 
-    Record(reducedPath, A, B, C)
+    val tokenisedPath = path.split(",").sliding(2, 2).map {
+      case Array(x, y) => (x,y)
+    }.toSeq
+
+    println(s"******************* $tokenisedPath")
+
+    val encodings = for {
+      aLength <- (1 to 5)
+      bLength <- (1 to 5)
+      a = tokenisedPath.take(aLength)
+      b = tokenisedPath.slice(aLength, aLength + bLength)
+      c = mopUp(a, b, tokenisedPath)
+      route = reducePath(a, b, c, tokenisedPath)
+      if(route.isDefined)
+      //each expression needs to be less than 20 chars
+      //that means 5 pairs (R,4) with commas between pairs -> 5*3 + 4
+      if(a.length <= 5 && b.length <= 5 && c.length <= 5 && route.get.length <= 10)
+    } yield (
+      a.map(l => s"${l._1},${l._2}").mkString(","),
+      b.map(l => s"${l._1},${l._2}").mkString(","),
+      c.map(l => s"${l._1},${l._2}").mkString(","),
+      route.get.mkString(","))
+
+
+    println(s"*****************There are ${encodings.length} encodings")
+    println(s"*****************encodings: $encodings")
+
+    Record(encodings.head._4, encodings.head._1, encodings.head._2, encodings.head._3)
   }
 }
